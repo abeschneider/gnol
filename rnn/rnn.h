7e77c1eef884d32ea98037b131153d6578be37ad
Abe Schneider <abe.schneider@gmail.com>
2015-08-26 12:39:34 -0400

    Initial Commit

---
+++ rnn/rnn.h
@@ -0,0 +1,23 @@
+/*
+ *  rnn.h
+ *  rnn
+ *
+ *  Created by Lais Washington on 8/26/15.
+ *  Copyright (c) 2015 Abraham Schneider. All rights reserved.
+ *
+ */
+
+#ifndef rnn_
+#define rnn_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class rnn
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
